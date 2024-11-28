87b3d5f352eb977120d049f0f214b3dc60d88ea5
Runa <runa.uxlabs@gmail.com>
2024-11-28 15:45:35 +0900

    insex 기본 설정 및 css 보완

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,14 +1,15 @@
 /* eslint-disable react/prop-types */
 function Counter({ index, count, onIncrease, onDecrease }) {
   return (
-    <div style={{ marginBottom: "10px" }}>
-      <span style={{ marginRight: "10px" }}>
-        Counter {index + 1}: {count}
+    <div className="counter">
+      <span>
+        {index + 1}번 카운터:
       </span>
-      <button onClick={() => onIncrease(index)}>+</button>
-      <button onClick={() => onDecrease(index)} style={{ marginLeft: "5px" }}>
-        -
-      </button>
+      <span>
+        {count}
+      </span>
+      <button onClick={() => onIncrease(index)}> + </button>
+      <button onClick={() => onDecrease(index)}> - </button>
     </div>
   );
 }
