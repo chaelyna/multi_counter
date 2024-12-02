0a8f0591b5401eb743f0c1ab0907bffd296276ee
Runa <runa.uxlabs@gmail.com>
2024-12-02 11:24:47 +0900

    여러 셋트로 구성되어있는 함수의 로직에서 한번 정해놓은 함수가 재사용되는 법 주석 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import Total from "./Total";
 import './App.css';
 
 function App() {
-  const [counters, setCounters] = useState([0, 0, 0]); // 카운터 상태 배열
+  // const [counters, setCounters] = useState([0, 0, 0]); // 카운터 상태 배열
+  const [counters, setCounters] = useState([100, 2]);
+  // useState() 훅에 초기값을 설정해서 내가 원하는 카운터 만들어 넣음
 
   // 특정 카운터 증가
   const increase = (index) => {
+    // 해당카운터의 인덱스 번호를 매개변수로 넣어 함수 실행
     setCounters((prev) =>
+      // setCounters()로 이전값(배열)이 들어오면, 그 값을 map()으로 돌려 값을 수정하고 부모의 상태값이 변경되도록 함
       prev.map((count, i) => (i === index ? count + 1 : count))
     );
   };
